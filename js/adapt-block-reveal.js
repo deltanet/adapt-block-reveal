940efe148a0feafddf598ec92c344b703d6fdf1f
Robert Peek <RobertPeek@users.noreply.github.com>
2020-10-06 10:25:08 +0100

    fw-v5 updates

--- js/adapt-block-reveal.js
+++ js/adapt-block-reveal.js
@@ -1,14 +1,14 @@
 define([
-    'core/js/adapt',
-    './openButtonView',
-    './closeButtonView'
-], function(Adapt, OpenButtonView, CloseButtonView) {
+  'core/js/adapt',
+  './openButtonView',
+  './closeButtonView'
+], function (Adapt, OpenButtonView, CloseButtonView) {
 
-    Adapt.on('articleView:postRender', function(view) {
-        if (view.model.get("_blockReveal") && view.model.get("_blockReveal")._isEnabled) {
-          new OpenButtonView({model:view.model});
-          new CloseButtonView({model:view.model});
-        }
-    });
+  Adapt.on('articleView:postRender', function (view) {
+    if (view.model.get('_blockReveal') && view.model.get('_blockReveal')._isEnabled) {
+      new OpenButtonView({model:view.model});
+      new CloseButtonView({model:view.model});
+    }
+  });
 
 });
--- js/openButtonView.js
+++ js/openButtonView.js
@@ -1,113 +1,114 @@
 define([
-    'core/js/adapt'
-], function(Adapt) {
+  'core/js/adapt'
+], function (Adapt) {
 
-    var OpenButtonView = Backbone.View.extend({
+  var OpenButtonView = Backbone.View.extend({
 
-        className: "block-reveal",
+    className: 'blockreveal',
 
-        events: {
-            "click .block-reveal-graphic-button": "openPopup",
-            "click .reveal-button": "openPopup"
-        },
+    events: {
+      'click .js-blockreveal-reveal-btn': 'openPopup'
+    },
 
-        initialize: function () {
-            this.listenTo(Adapt, 'remove', this.remove);
-            this.listenTo(this.model, 'change:_isComplete', this.updateIcon);
-            this.render();
-        },
+    initialize: function () {
+      this.listenTo(Adapt, 'remove', this.remove);
+      this.listenTo(Adapt, 'pageView:ready', this.render);
+      this.listenTo(this.model, 'change:_isComplete', this.updateIcon);
+    },
 
-        render: function () {
-            var data = this.model.toJSON();
-            var template = Handlebars.templates["openButton"];
+    render: function () {
+      var data = this.model.toJSON();
+      var template = Handlebars.templates['openButton'];
 
-            // Collect data and set up block id's
-            this.hideBlockNum = this.model.get('_blockReveal')._blockToHide._number;
-            this.revealBlockNum = this.model.get('_blockReveal')._blockToReveal._number;
+      // Collect data and set up block id's
+      this.hideBlockNum = this.model.get('_blockReveal')._blockToHide._number;
+      this.revealBlockNum = this.model.get('_blockReveal')._blockToReveal._number;
 
-            // Get children and create array
-            this.children = this.model.getChildren(true);
-            this.childrenId = new Array();
-            for (var i = 0, l = this.children.length; i < l; i++) {
-              this.childrenId[i] = this.children.models[i].get('_id');
-            }
+      // Get children and create array
+      this.children = this.model.getChildren(true);
+      this.childrenId = [];
+      for (var i = 0, l = this.children.length; i < l; i++) {
+        this.childrenId[i] = this.children.models[i].get('_id');
+      }
 
-            // Backward compatible
-            // If length of number is greater than 5 it will be treated as a string ID
-            // Set id for block to hide
-            if(this.hideBlockNum.length > 5) {
-              this.blockToHide = this.hideBlockNum;
-            } else {
-              this.blockToHide = this.childrenId[this.hideBlockNum-1];
-            }
-            // Set id for block to reveal
-            if(this.revealBlockNum.length > 5) {
-              this.blockToReveal = this.revealBlockNum;
-            } else {
-              this.blockToReveal = this.childrenId[this.revealBlockNum-1];
-            }
+      // Backward compatible
+      // If length of number is greater than 5 it will be treated as a string ID
+      // Set id for block to hide
+      if (this.hideBlockNum.length > 5) {
+        this.blockToHide = this.hideBlockNum;
+      } else {
+        this.blockToHide = this.childrenId[this.hideBlockNum-1];
+      }
+      // Set id for block to reveal
+      if (this.revealBlockNum.length > 5) {
+        this.blockToReveal = this.revealBlockNum;
+      } else {
+        this.blockToReveal = this.childrenId[this.revealBlockNum-1];
+      }
 
-            $(this.el).html(template(data)).appendTo('.' + this.blockToHide + " > .block-inner");
+      $(this.el).html(template(data)).appendTo('.'+this.blockToHide+'>.block__inner');
 
-            var $blockToHideInner = $("." + this.blockToHide);
-            var $blockToRevealInner = $("." + this.blockToReveal);
+      $(this.el).addClass(this.model.get('_blockReveal')._blockToHide._buttonLocation + ' ' +this.model.get('_blockReveal')._blockToHide._classes);
 
-            $blockToRevealInner.addClass('block-reveal block-reveal-hidden');
-            $blockToHideInner.css('opacity', 1);
-            $blockToRevealInner.css('opacity', 0);
+      var $blockToHideInner = $('.' + this.blockToHide);
+      var $blockToRevealInner = $('.' + this.blockToReveal);
 
-            if (this.model.get('_blockReveal')._trackCompletion){
-                if (!this.model.get('_isComplete')){
-                    this.$('.block-reveal-button').hide();
-                }
-            }
+      $blockToRevealInner.addClass('is-blockreveal-hidden');
+      $blockToHideInner.css('opacity', 1);
+      $blockToRevealInner.css('opacity', 0);
 
-            // Resize title to match button
-            if (this.model.get('_blockReveal')._blockToHide._buttonLocation == "bottom") return;
-
-            var $titleElement = $('.'+this.blockToHide).find('.block-title');
-            var $titleInnerElement = $('.'+this.blockToHide).find('.block-title-inner');
-
-            if ($titleElement.length) {
-              var titlePadding = $titleElement.outerHeight() - $titleElement.height();
-              $titleElement.css('min-height', this.$('button').outerHeight() - titlePadding);
-              $titleInnerElement.css('vertical-align', 'middle');
-            }
-        },
-
-        openPopup: function(event) {
-            if (event) event.preventDefault();
-
-            Adapt.trigger('audio:pauseAudio', 0);
-
-            var $blockToHideInner = $("." + this.blockToHide);
-            var $blockToRevealInner = $("." + this.blockToReveal);
-
-            this.$('.block-reveal-graphic-button').addClass("visited");
-
-            $blockToHideInner.addClass('block-reveal-hidden');
-            $blockToRevealInner.removeClass('block-reveal-hidden');
-
-            $blockToHideInner.velocity({
-                opacity: 0
-            });
-
-            $blockToRevealInner.velocity({
-                opacity: 1
-            });
-
-            Adapt.scrollTo("." + this.blockToReveal, { duration:400 });
-            $(window).resize();
-        },
-
-        updateIcon: function () {
-            if(this.model.get('_isComplete')){
-                this.$('.block-reveal-button').show();
-            }
+      if (this.model.get('_blockReveal')._trackCompletion){
+        if (!this.model.get('_isComplete')){
+          this.$('.blockreveal__btn').hide();
         }
+      }
 
-    });
+      // Resize title to match button
+      if (this.model.get('_blockReveal')._blockToHide._buttonLocation == 'bottom') return;
 
-    return OpenButtonView;
+      var $titleElement = $('.'+this.blockToHide).find('.block__title');
+      var $titleInnerElement = $('.'+this.blockToHide).find('.block__title-inner');
+
+      if ($titleElement.length) {
+        var titlePadding = $titleElement.outerHeight() - $titleElement.height();
+        $titleElement.css('min-height', this.$('button').outerHeight() - titlePadding);
+        $titleInnerElement.css('vertical-align', 'middle');
+      }
+    },
+
+    openPopup: function (event) {
+      if (event) event.preventDefault();
+
+      Adapt.trigger('audio:pauseAudio', 0);
+
+      var $blockToHideInner = $('.' + this.blockToHide);
+      var $blockToRevealInner = $('.' + this.blockToReveal);
+
+      this.$('.blockreveal-graphic-btn').addClass('visited');
+
+      $blockToHideInner.addClass('is-blockreveal-hidden');
+      $blockToRevealInner.removeClass('is-blockreveal-hidden');
+
+      $blockToHideInner.velocity({
+        opacity: 0
+      });
+
+      $blockToRevealInner.velocity({
+        opacity: 1
+      });
+
+      Adapt.scrollTo('.' + this.blockToReveal, { duration:400 });
+      $(window).resize();
+    },
+
+    updateIcon: function () {
+      if (this.model.get('_isComplete')){
+        this.$('.blockreveal__btn').show();
+      }
+    }
+
+  });
+
+  return OpenButtonView;
 
 });
